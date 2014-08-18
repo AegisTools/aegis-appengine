f4b0586ea0d4e4aac472dbf8cf361a75ccde5c6f
Paul Hounshell <paul.hounshell@gmail.com>
2014-08-18 11:58:19 -0700

    More permission unit test cleanup.

--- tests/api/permissions.py
+++ tests/api/permissions.py
@@ -39,59 +39,21 @@ class PermissionTests(common.AegisTestCase):
         self.assertTrue(self.get("test_harness/permissions/user/action/kind/a/b/c", USER_ROOT).json()["allowed"])
 
 
-#     def test_root_grant_permission_with_id(self):
-#         self.assertEqual(200, self.put("users/a", USER_ROOT).status_code)
-#         self.assertEqual(200, self.put("users/a/permissions/b/c/d").status_code)
-# 
-#         list = self.get("users/a/permissions", USER_ROOT).json()
-#         self.assertEqual(1, len(list))
-#         self.assertEqual("a", list[0]["user"])
-#         self.assertEqual("b", list[0]["type"])
-#         self.assertEqual("c", list[0]["action"])
-#         self.assertEqual("d", list[0]["id"])
-# 
-# 
-#     def test_root_grant_2_permissions(self):
-#         self.assertEqual(200, self.put("users/a", USER_ROOT).status_code)
-#         self.assertEqual(200, self.put("users/a/permissions/b/c").status_code)
-#         self.assertEqual(200, self.put("users/a/permissions/b/d").status_code)
-# 
-#         list = self.get("users/a/permissions", USER_ROOT).json()
-#         self.assertEqual(2, len(list))
-#         self.assertEqual("a", list[0]["user"])
-#         self.assertEqual("b", list[0]["type"])
-#         self.assertEqual("a", list[1]["user"])
-#         self.assertEqual("b", list[1]["type"])
-#         self.assertEqual(set([ "c", "d" ]), set([ list[0]["action"], list[1]["action"] ]))
-# 
-# 
-#     def test_root_grant_2_id_permissions(self):
-#         self.assertEqual(200, self.put("users/a", USER_ROOT).status_code)
-#         self.assertEqual(200, self.put("users/a/permissions/b/c/d").status_code)
-#         self.assertEqual(200, self.put("users/a/permissions/b/c/e").status_code)
-# 
-#         list = self.get("users/a/permissions", USER_ROOT).json()
-#         self.assertEqual(2, len(list))
-#         self.assertEqual("a", list[0]["user"])
-#         self.assertEqual("b", list[0]["type"])
-#         self.assertEqual("c", list[0]["action"])
-#         self.assertEqual("a", list[1]["user"])
-#         self.assertEqual("b", list[1]["type"])
-#         self.assertEqual("c", list[0]["action"])
-#         self.assertEqual(set([ "d", "e" ]), set([ list[0]["id"], list[1]["id"] ]))
-# 
-# 
-#     def test_root_revoke_permissions(self):
-#         self.assertEqual(200, self.put("users/a", USER_ROOT).status_code)
-#         self.assertEqual(200, self.put("users/a/permissions/b/c").status_code)
-#         self.assertEqual(200, self.put("users/a/permissions/b/d").status_code)
-#         self.assertEqual(200, self.delete("users/a/permissions/b/d").status_code)
-# 
-#         list = self.get("users/a/permissions", USER_ROOT).json()
-#         self.assertEqual(1, len(list))
-#         self.assertEqual("a", list[0]["user"])
-#         self.assertEqual("b", list[0]["type"])
-#         self.assertEqual("c", list[0]["action"])
+    def test_root_revoke_permission(self):
+        self.assertEqual(200, self.put("test_harness/permissions/user/action/kind", USER_ROOT).status_code)
+        self.assertEqual(200, self.delete("test_harness/permissions/user/action/kind", USER_ROOT).status_code)
+        self.assertFalse(self.get("test_harness/permissions/user/action/kind", USER_ROOT).json()["allowed"])
+
+
+    def test_root_revoke_parent_permission(self):
+        self.assertEqual(200, self.put("test_harness/permissions/user/action/kind", USER_ROOT).status_code)
+        self.assertEqual(200, self.put("test_harness/permissions/user/action/kind/a/b/c", USER_ROOT).status_code)
+        self.assertEqual(200, self.delete("test_harness/permissions/user/action/kind", USER_ROOT).status_code)
+        self.assertFalse(self.get("test_harness/permissions/user/action/kind", USER_ROOT).json()["allowed"])
+        self.assertFalse(self.get("test_harness/permissions/user/action/kind/a", USER_ROOT).json()["allowed"])
+        self.assertTrue(self.get("test_harness/permissions/user/action/kind/a/b/c", USER_ROOT).json()["allowed"])
+        self.assertTrue(self.get("test_harness/permissions/user/action/kind/a/b/c/d", USER_ROOT).json()["allowed"])
+
 
 
 if __name__ == "__main__":
