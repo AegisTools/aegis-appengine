5e2bc488b6e2ebbf29311077426dead1f6d05d62
Paul Hounshell <paul.hounshell@gmail.com>
2014-09-02 20:18:13 -0700

    Disable test_harness module in production environment

--- modules/test_harness/__init__.py
+++ modules/test_harness/__init__.py
@@ -17,114 +17,119 @@ from common import wipe
 log = logging.getLogger("test_harness")
 
 
-dependencies = ["users"]
-
-"""
-GENERAL PURPOSE STUBS
-"""
-
-def wipe(viewer, **ignored):
-    log.error("Wiping entire database")
-    ndb.delete_multi(ndb.Query().iter(keys_only = True))
-    # db.delete(db.Query(keys_only=True))
-    memcache.flush_all()
-
-"""
-ISSUE STUBS
-"""
-
-def issue_permission(viewer, user=None):
-    permission_grant(viewer, user or viewer, "issue", "read")
-    permission_grant(viewer, user or viewer, "issue", "create")
-    permission_grant(viewer, user or viewer, "issue", "update")
-    permission_grant(viewer, user or viewer, "user", "read")
-
-"""
-PERMISSION STUBS
-"""
-
-def grant_permission(viewer, user, kind, action, thing=[], **ignored):
-    key = build_thing_key(kind, thing)
-    permission_grant(viewer, user, kind, action, key)
+if not os.environ.get('SERVER_SOFTWARE','').startswith('Development'):
+    log.debug("Production environment detectedl test harness disabled")
+else:
+    log.warn("Development environment detected; test harness enabled")
 
 
-def revoke_permission(viewer, user, kind, action, thing=[], **ignored):
-    key = build_thing_key(kind, thing)
-    permission_revoke(viewer, user, kind, action, key)
+    dependencies = ["users"]
+
+    """
+    GENERAL PURPOSE STUBS
+    """
+
+    def wipe(viewer, **ignored):
+        log.error("Wiping entire database")
+        ndb.delete_multi(ndb.Query().iter(keys_only = True))
+        # db.delete(db.Query(keys_only=True))
+        memcache.flush_all()
+
+    """
+    ISSUE STUBS
+    """
+
+    def issue_permission(viewer, user=None):
+        permission_grant(viewer, user or viewer, "issue", "read")
+        permission_grant(viewer, user or viewer, "issue", "create")
+        permission_grant(viewer, user or viewer, "issue", "update")
+        permission_grant(viewer, user or viewer, "user", "read")
+
+    """
+    PERMISSION STUBS
+    """
+
+    def grant_permission(viewer, user, kind, action, thing=[], **ignored):
+        key = build_thing_key(kind, thing)
+        permission_grant(viewer, user, kind, action, key)
 
 
-def check_permission(viewer, keys):
-    log.debug(keys)
-    if "thing" in keys:
-        key = build_thing_key(keys["kind"], keys["thing"])
-    else:
-        key = build_thing_key(keys["kind"], [])
-
-    if permission_check(keys["user"], keys["kind"], keys["action"], key):
-        return { "allowed": True }
-    else:
-        return { "allowed": False }
-
-"""
-TAG STUBS
-"""
-
-def apply_tag(viewer, thing, tag, **ignored):
-    tag_apply(viewer, ndb.Key('Thing', thing), tag)
+    def revoke_permission(viewer, user, kind, action, thing=[], **ignored):
+        key = build_thing_key(kind, thing)
+        permission_revoke(viewer, user, kind, action, key)
 
 
-def remove_tag(viewer, thing, tag, **ignored):
-    tag_remove(viewer, ndb.Key('Thing', thing), tag)
+    def check_permission(viewer, keys):
+        log.debug(keys)
+        if "thing" in keys:
+            key = build_thing_key(keys["kind"], keys["thing"])
+        else:
+            key = build_thing_key(keys["kind"], [])
+
+        if permission_check(keys["user"], keys["kind"], keys["action"], key):
+            return { "allowed": True }
+        else:
+            return { "allowed": False }
+
+    """
+    TAG STUBS
+    """
+
+    def apply_tag(viewer, thing, tag, **ignored):
+        tag_apply(viewer, ndb.Key('Thing', thing), tag)
 
 
-def get_tags(viewer, thing):
-    return tag_list(viewer, ndb.Key('Thing', thing))
-
-"""
-REMARK STUBS
-"""
-
-def remark(viewer, thing):
-    remark_create(viewer, ndb.Key('Thing', thing), "This is a dummy remark")
+    def remove_tag(viewer, thing, tag, **ignored):
+        tag_remove(viewer, ndb.Key('Thing', thing), tag)
 
 
-def get_remarks(viewer, thing):
-    return remark_list(viewer, ndb.Key('Thing', thing))
+    def get_tags(viewer, thing):
+        return tag_list(viewer, ndb.Key('Thing', thing))
 
-"""
-HELPER METHODS
-"""
+    """
+    REMARK STUBS
+    """
 
-def build_thing_key(kind, id):
-    key = None
-    for chunk in id:
-        key = ndb.Key(kind, chunk, parent=key)
-        log.debug(key)
-    return key
+    def remark(viewer, thing):
+        remark_create(viewer, ndb.Key('Thing', thing), "This is a dummy remark")
 
 
+    def get_remarks(viewer, thing):
+        return remark_list(viewer, ndb.Key('Thing', thing))
+
+    """
+    HELPER METHODS
+    """
+
+    def build_thing_key(kind, id):
+        key = None
+        for chunk in id:
+            key = ndb.Key(kind, chunk, parent=key)
+            log.debug(key)
+        return key
 
 
-templates = { "tag/{thing}"                                  : "tag_list",
-              "permissions/{user}/{action}/{kind}"           : "permission_view",
-              "permissions/{user}/{action}/{kind}/{thing}/*" : "permission_view",
-              "remarks/{thing}"                              : "remarks_view" }
 
 
-types = { "permission" : check_permission,
-          "tags"       : get_tags,
-          "remarks"    : get_remarks }
+    templates = { "tag/{thing}"                                  : "tag_list",
+                "permissions/{user}/{action}/{kind}"           : "permission_view",
+                "permissions/{user}/{action}/{kind}/{thing}/*" : "permission_view",
+                "remarks/{thing}"                              : "remarks_view" }
 
 
-actions = { "wipe"                                         : { "POST"   : { "method" : wipe } },
-            "tag/{thing}/{tag}/*"                          : { "PUT"    : { "method" : apply_tag },
-                                                               "DELETE" : { "method" : remove_tag } },
-            "permissions/{user}/{action}/{kind}"           : { "PUT"    : { "method" : grant_permission },
-                                                               "DELETE" : { "method" : revoke_permission } },
-            "permissions/{user}/{action}/{kind}/{thing}/*" : { "PUT"    : { "method" : grant_permission } },
-            "remarks/{thing}"                              : { "POST"   : { "method" : remark } },
-            "issues/permission"                            : { "PUT"    : { "method" : issue_permission } } }
+    types = { "permission" : check_permission,
+            "tags"       : get_tags,
+            "remarks"    : get_remarks }
 
 
-log.warn("Test harness is enabled")
+    actions = { "wipe"                                         : { "POST"   : { "method" : wipe } },
+                "tag/{thing}/{tag}/*"                          : { "PUT"    : { "method" : apply_tag },
+                                                                "DELETE" : { "method" : remove_tag } },
+                "permissions/{user}/{action}/{kind}"           : { "PUT"    : { "method" : grant_permission },
+                                                                "DELETE" : { "method" : revoke_permission } },
+                "permissions/{user}/{action}/{kind}/{thing}/*" : { "PUT"    : { "method" : grant_permission } },
+                "remarks/{thing}"                              : { "POST"   : { "method" : remark } },
+                "issues/permission"                            : { "PUT"    : { "method" : issue_permission } } }
+
+
 
