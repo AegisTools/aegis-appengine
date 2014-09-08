769ef468b5e7426da45518a4a84e1c1974b296b8
Paul Hounshell <paul.hounshell@gmail.com>
2014-09-08 12:05:55 -0700

    Email bug

--- modules/errors/__init__.py
+++ modules/errors/__init__.py
@@ -7,7 +7,7 @@ def error_report(viewer, url=None, request_code=None, description=None, stack=No
     body = "%s\n\nURL: %s\nRequest: %s\n\n%s" % (description, url, request_code, stack)
     log.debug("Sending Email to Admins:")
     log.debug(body)
-    mail.send_mail_to_admins(viewer, "Aegis Error - %s" % url, body)
+    mail.send_mail_to_admins(viewer.email(), "Aegis Error - %s" % url, body)
 
 
 templates = { "{issue_id}"      : "view",
