62d007b94e2fa4734a90c1b74dc7293e7536906c
Paul Hounshell <paul.hounshell@gmail.com>
2014-09-10 13:53:05 -0700

    Fix error email sender

--- modules/errors/__init__.py
+++ modules/errors/__init__.py
@@ -1,13 +1,22 @@
 import logging
 from google.appengine.api import mail
 
+sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
+import system_settings
+
 log = logging.getLogger("issues")
 
 def error_report(viewer, url=None, request_code=None, description=None, stack=None, **ignored):
+    settings = settings or system_settings.get_system_settings()
+
+    sender = viewer.email()
+    if "email_admin" in settings:
+        sender = settings["email_admin"]
+    
     body = "%s\n\nURL: %s\nRequest: %s\n\n%s" % (description, url, request_code, stack)
-    log.debug("Sending Email to Admins from %s:" % viewer.email())
+    log.debug("Sending Email to Admins from %s:" % sender)
     log.debug(body)
-    mail.send_mail_to_admins(viewer.email(), "Aegis Error - %s" % url, body)
+    mail.send_mail_to_admins(sender, "Aegis Error - %s" % url, body)
 
 
 actions = { "report"     : { "POST"   : { "method"   : error_report,
